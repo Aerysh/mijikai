c96ac4c4b7b0b107007c428ff25529500e832af9
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-12-30 12:55:54 +0700

    feat: add database configurationsq

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      status: 'success',
+      message: 'Welcome to mijikai',
+      version: '0.0.1',
+      url: 'https://github.com/Aerysh/mijikai',
+    };
   }
 }
